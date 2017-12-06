2b43ad632c0de54194d84d51d35b7b34ed61e46d
sharath Mankala <sharath.mankala@gmail.com>
2017-12-06 17:44:52 +0530

    Added README.md math.java

--- math.java
+++ math.java
@@ -1,8 +1,6 @@
-//THis is my Firsh Java Program
-//sharath mankala
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
              System.out.println("Hello, World");
                 }
-                }
+      :          }
